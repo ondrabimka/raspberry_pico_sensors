a2e24b5386be24e64d42b223b6327e64499a66eb
ondrabimka <ondrabimka@seznam.cz>
2024-04-19 14:16:27 +0200

    new EKG image

--- raspberry_pico_sensors/soil_moisture/main.py
+++ raspberry_pico_sensors/soil_moisture/main.py
@@ -2,22 +2,21 @@ import machine
 import utime
 
 sensor_pin = machine.Pin(28)  # Change this to the correct pin number
-adc = machine.ADC(sensor_pin)
 
-def read_soil_moisture():
-    # Read analog value from ADC
-    output = adc.read_u16()
-
-    # Print raw analog value for troubleshooting
-    print("Raw Analog Value:", output)
-
-# Main loop to continuously read and print soil moisture values
 try:
-    print("Reading From the Sensor ...")
+    print("Reading Soil Moisture...")
     utime.sleep(2)
 
     while True:
-        read_soil_moisture()
+        
+        adc = machine.ADC(sensor_pin)
+        raw_value = adc.read_u16()
+
+        # Convert the raw ADC reading to a percentage value (assuming a linear relationship)
+        # Adjust the mapping based on your sensor's characteristics
+        # Replace 0 and 65535 with the minimum and maximum raw readings you get from your sensor
+        moisture_percentage = ((raw_value - 0) / (65535 - 0)) * 100
+        print("Soil Moisture:", "{:.2f}%".format(moisture_percentage))
         utime.sleep(1)
 
 except KeyboardInterrupt:
