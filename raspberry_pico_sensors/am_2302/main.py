b699d30c0d486cf41fec9c4b0426ef49d9a09ab9
ondrabimka <ondrabimka@seznam.cz>
2024-10-24 18:57:09 +0200

    am_2302

---
+++ raspberry_pico_sensors/am_2302/main.py
@@ -0,0 +1,14 @@
+from machine import Pin
+from time import sleep
+import dht
+
+sensor = dht.DHT22(Pin(13))
+
+while True:
+  try:
+    sensor.measure()     # Recovers measurements from the sensor
+    print(f"Temperature : {sensor.temperature():.1f}°C")
+    print(f"Humidity    : {sensor.humidity():.1f}%")
+    sleep(2)             # the DHT22 returns at most one measurement every 2s, so we wait 2 seconds before reading again
+  except OSError as e:
+    print("Failed reception")
